e049633807a361490e888099e6ec9400075935b8
Thabung <thabungm@gmail.com>
2022-08-16 12:50:02 +0530

    keyboard event handler

---
+++ javascript/usingJavascriptWithWeb/querySelector/index.js
@@ -0,0 +1,14 @@
+/* console.log(document.getElementById('container1'));
+console.log(document.querySelector('#container1'));
+console.log(document.querySelector('.container2'));
+ */
+
+function onKeyDownHandler(event) {
+  const { key, code } = event;
+  console.log(`Key: ${key}; Code: ${code}`);
+}
+
+function onKeyUpHandler(event) {
+  /*  console.log('onKeyup handler');
+  console.log(event); */
+}
